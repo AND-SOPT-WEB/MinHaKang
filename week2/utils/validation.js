c64f02b555734f7aaa13ba9db75a82756e7050ab
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-01 19:04:47 +0900

    refactor: 입력값 검증로직 수정
    - 코드리뷰 반영

--- week2/utils/validation.js
+++ week2/utils/validation.js
@@ -1,26 +1,10 @@
 // 모달폼의 입력값 검증
 export const isFormValid = (member) => {
-  const {
-    name,
-    englishName,
-    github,
-    gender,
-    role,
-    firstWeekGroup,
-    secondWeekGroup,
-  } = member;
-
-  if (
-    !name ||
-    !englishName ||
-    !github ||
-    !gender ||
-    !role ||
-    firstWeekGroup === null ||
-    secondWeekGroup === null
-  ) {
+  const isValid = Object.values(member).every(
+    (value) => value !== null && value !== ''
+  );
+  if (!isValid) {
     alert('모든 값을 입력해주세요.');
-    return false;
   }
-  return true;
+  return isValid;
 };
