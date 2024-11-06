93638bab3a46a74e2ad4ffc5381830ad8e98121d
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-07 01:27:31 +0900

    feat: `Button` 컴포넌트 추가

---
+++ week4/src/components/button/Button.css.ts
@@ -0,0 +1,17 @@
+import { vars } from '@styles/theme.css';
+import { style } from '@vanilla-extract/css';
+
+export const button = style({
+  marginBottom: 14,
+  padding: 12,
+  borderRadius: 8,
+  backgroundColor: vars.color.primary,
+  color: vars.color.white,
+  textAlign: 'center',
+  fontSize: vars.fontSize.md,
+
+  ':hover': {
+    backgroundColor: vars.color.blue600,
+    transition: 'background-color 0.3s',
+  },
+});
